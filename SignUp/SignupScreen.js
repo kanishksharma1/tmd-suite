eaf7d8aeaca36a87efca116aecdf56d975d41b00
kanishk sharma <julupandit@gmail.com>
2024-07-03 01:33:38 +0530

    signup-changes

--- SignUp/SignupScreen.js
+++ SignUp/SignupScreen.js
@@ -11,60 +11,43 @@ export default function SignupScreen() {
 
   return (
     <ScrollView contentContainerStyle={styles.scrollView}>
-<View style={styles.imageDiv}>
-          <Image
-            source={require('/home/tmd-pc/react-native-demo/react-native/react-native-firstApp/assets/6963-ai.png')}
-            style={styles.image}
-            resizeMode="contain"
-          />
-        </View>
-
-
+      <View style={styles.imageDiv}>
+        <Image
+          source={require('C:/Users/LENOVO/tmd-suite/assets/6963-ai.png')}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View>
       <View style={styles.container}>
         <View style={styles.innerContainer}>
           <Text style={styles.title}>Create Your Account</Text>
           <InputComponent placeholder="Name" label="Enter Full Name" />
           <InputComponent placeholder="Store Name" />
-          <InputComponent placeholder="Mobile Number" keyboardType="numeric" />
-          <InputComponent placeholder="Enter OTP" getOTP keyboardType="phone-pad"/>
+          <InputComponent placeholder="Mobile Number"  getOTP keyboardType="numeric" />
+          <InputComponent placeholder="Enter OTP"  keyboardType="phone-pad" />
           <InputComponent placeholder="Password" secureTextEntry />
           <View style={styles.checkboxContainer}>
-            
-            {/* <CheckBox
-              checked={checked}
+            <Ionicons 
+              name={checked ? "checkbox" : "square-outline"} 
+              size={24} 
+              color={checked ? "#0066b0" : "#00adef"} 
               onPress={() => setChecked(!checked)}
-              checkedColor="#00adef"
-              uncheckedColor="#00adef"
-              // containerStyle={styles.checkbox}
-              // textStyle={styles.checkboxText}
-            /> */}
-        <Ionicons 
-        name={checked ? "checkbox" : "square-outline"} 
-        size={24} 
-        color={checked ? "#0066b0" : "#00adef"} 
-        onPress={() => setChecked(!checked)}
-        style={StyleSheet.create({})}
-      />
-            
+            />
             <Text style={styles.checkboxLabel}>
               I agree to the tmd Suite{' '}
               <Text style={styles.linkText}>terms & conditions</Text> 
             </Text>
           </View>
-          <SignupButton buttonText="Sign Up" disabled={!checked}/>
+          <SignupButton buttonText="Sign Up" disabled={!checked} />
         </View>
       </View>
-
-
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   scrollView: {
-    paddingTop:30,
     flexGrow: 1,
-    justifyContent: 'center',
     backgroundColor: '#ffffff',
   },
   container: {
@@ -72,67 +55,55 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 15,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: '#ffffff',
+    marginTop: -20, // Adjust if necessary to ensure seamless appearance with imageDiv
   },
   innerContainer: {
     width: '100%',
-    paddingTop:0,
-    paddingLeft:20,
-    paddingRight:20,
-    paddingBottom:20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 20,
     backgroundColor: '#ffffff',
-    // borderRadius: 10,
-    // shadowColor: '#000',
-    // shadowOffset: { width: 0, height: 2 },
-    // shadowOpacity: 0.2,
-    // shadowRadius: 2,
-    // elevation: 3,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
   },
   title: {
     color: '#0066b0',
     fontSize: 22,
     fontWeight: '600',
     textAlign: 'center',
-    paddingBottom: 20,
+    paddingTop:15,
+    paddingBottom: 30,
   },
   checkboxContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    // paddingTop: 10,
-    paddingBottom: 20,
-    // paddingRight:40,
-    // position:'relative',
-    // right:40
-  },
-  checkbox: {
-    backgroundColor: '#00adef',
-    borderColor: '#00adef',
-    borderRadius: 5,
-    marginRight: 10,
-  },
-  checkboxText: {
-    color: '#fff',
+    paddingBottom: 25,
+    paddingTop: 10,
   },
   checkboxLabel: {
     color: 'grey',
-    lineHeight:25,
-    paddingLeft:10,
+    lineHeight: 25,
+    paddingLeft: 10,
+
   },
   linkText: {
     color: '#0066b0',
     fontWeight: '600',
-    
   },
   imageDiv: {
     width: '100%',
-    height: 150,
+    height: 250,
     backgroundColor: '#0066b0',
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 20,
+    position: 'relative',
+    top:19
   },
   image: {
-    width: '100%',
-    height: '100%',
+    width: '85%',
+    height: '70%',
   },
 });
-
